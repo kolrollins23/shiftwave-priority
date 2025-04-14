62f1a70ba23226e446bab4b4ab64661ee8c0fcf6
Kol Rollins <KolRollins@MacBook-Air-158.local>
2025-04-14 16:18:37 -0400

    Private Form

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://shiftwavesales.vercel.app"
+        "https://shiftwavesales.vercel.app/privateform"
     ],
     allow_credentials=True,
     allow_methods=["*"],
